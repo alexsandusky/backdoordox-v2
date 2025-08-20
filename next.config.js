cd826cd81a59da503531ed9919d0fccde246429c
alexsandusky <alexsandusky@AS2190>
2025-08-20 23:12:58 +0800

    chore: remove unused experimental flag

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true
-  },
-  experimental: {
-    reactCompiler: false
-  },
-  headers: async () => {
-    return [
-      {
-        source: "/view/:path*",
-        headers: [
-          { key: "X-Frame-Options", value: "SAMEORIGIN" },
-          { key: "Referrer-Policy", value: "no-referrer" }
-        ]
-      }
-    ]
-  }
+  images: { unoptimized: true },
+  headers: async () => ([
+    {
+      source: "/view/:path*",
+      headers: [
+        { key: "X-Frame-Options", value: "SAMEORIGIN" },
+        { key: "Referrer-Policy", value: "no-referrer" },
+      ],
+    },
+  ]),
 }
-module.exports = nextConfig;
+module.exports = nextConfig
